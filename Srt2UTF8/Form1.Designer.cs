da29a6d831a8600f2a9626c1c3fe29e8fb94a9c3
SanCoder-Q <sancoder.q@gmail.com>
2014-09-25 09:00:39 +0800

    First version

---
+++ Srt2UTF8/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Srt2UTF8
+{
+    partial class Srt2UTF8Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Srt2UTF8Form));
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.GoBtn = new System.Windows.Forms.Button();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.FileTypeTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.Location = new System.Drawing.Point(445, 12);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenBtn.TabIndex = 0;
+            this.OpenBtn.Text = "Open";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // GoBtn
+            // 
+            this.GoBtn.Enabled = false;
+            this.GoBtn.Location = new System.Drawing.Point(445, 41);
+            this.GoBtn.Name = "GoBtn";
+            this.GoBtn.Size = new System.Drawing.Size(75, 23);
+            this.GoBtn.TabIndex = 1;
+            this.GoBtn.Text = "Go";
+            this.GoBtn.UseVisualStyleBackColor = true;
+            this.GoBtn.Click += new System.EventHandler(this.GoBtn_Click);
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.Location = new System.Drawing.Point(12, 14);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.ReadOnly = true;
+            this.UrlTextBox.Size = new System.Drawing.Size(427, 21);
+            this.UrlTextBox.TabIndex = 2;
+            // 
+            // FileTypeTextBox
+            // 
+            this.FileTypeTextBox.Location = new System.Drawing.Point(12, 43);
+            this.FileTypeTextBox.Name = "FileTypeTextBox";
+            this.FileTypeTextBox.Size = new System.Drawing.Size(427, 21);
+            this.FileTypeTextBox.TabIndex = 3;
+            this.FileTypeTextBox.Text = "ass,srt";
+            // 
+            // Srt2UTF8Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 76);
+            this.Controls.Add(this.FileTypeTextBox);
+            this.Controls.Add(this.UrlTextBox);
+            this.Controls.Add(this.GoBtn);
+            this.Controls.Add(this.OpenBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Srt2UTF8Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Srt2UTF8 Made by SanCoder, 2014-09-02. SanCoder.Q@gmail.com ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.Button GoBtn;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.TextBox FileTypeTextBox;
+    }
+}
+
---
+++ Srt2UTF8/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using FileEncoding;
+
+namespace Srt2UTF8
+{
+    public partial class Srt2UTF8Form : Form
+    {
+        List<string> FileTypes = new List<string>();
+        List<FileInfo> FileInfoList = new List<FileInfo>();
+
+        public Srt2UTF8Form()
+        {
+            InitializeComponent();
+        }
+
+        private bool ReadFileType()
+        {
+            FileTypes.Clear();
+
+            string strTypes = FileTypeTextBox.Text;
+            char[] spliter = { ',', ';', '\t' };
+            string[] strType = strTypes.Split(spliter);
+
+            if (strType.Length == 0)
+            {
+                MessageBox.Show("File type cannot be empty!");
+                return false;
+            }
+
+            for (int i = 0; i < strType.Length; i++)
+            {
+                if (strType[i].Trim().Length > 0)
+                    FileTypes.Add(strType[i].Trim().ToLower());
+            }
+
+            if (FileTypes.Count == 0)
+            {
+                MessageBox.Show("File type cannot be empty!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            if(!ReadFileType())
+                return;
+            FileInfoList.Clear();
+
+            if (FileTypes.Count == 0)
+            {
+                MessageBox.Show("File type cannot be empty!");
+                return;
+            }
+
+            FolderBrowserDialog _ofd = new FolderBrowserDialog();
+
+            if (_ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            string strPath = _ofd.SelectedPath.Trim();
+
+            DirectoryInfo _di = new DirectoryInfo(strPath);
+
+            FileInfo[] fileInfos;
+
+            foreach (string type in FileTypes)
+            {
+                fileInfos = _di.GetFiles("*." + type, SearchOption.AllDirectories);
+                for (int i = 0; i < fileInfos.Length; i++)
+                {
+                    FileInfoList.Add(fileInfos[i]);
+                }
+            }
+
+            GoBtn.Enabled = true;
+        }
+
+        private void GoBtn_Click(object sender, EventArgs e)
+        {
+            IdentifyEncoding _ie = new IdentifyEncoding();
+
+            int sum = 0;
+            for (int i = 0; i < FileInfoList.Count; i++)
+            {
+                FileInfo info = FileInfoList[i];
+                //Encoding fileCode = EncodingType.GetType(info.FullName);
+                string code = _ie.GetEncodingName(info);
+                Console.WriteLine(code + "   " + info.FullName);
+
+                //转换GB2312/Big5/ASCII 到 UTF-8
+                FileStream stream;
+                Byte[] buffer;
+                Byte[] newBuff;
+                switch (code)
+                {
+                    case "GB2312":
+                        stream = info.OpenRead();
+                        buffer = new Byte[stream.Length];
+                        stream.Read(buffer, 0, Convert.ToInt32(stream.Length));
+                        stream.Close();
+                        stream.Dispose();
+                        newBuff = Encoding.Convert(Encoding.GetEncoding("gb2312"), Encoding.UTF8, buffer);
+                        info.Delete();
+                        stream = info.OpenWrite();
+                        stream.Write(newBuff, 0, newBuff.Length);
+                        stream.Close();
+                        sum++;
+                        break;
+                    case "Big5":
+                        stream = info.OpenRead();
+                        buffer = new Byte[stream.Length];
+                        stream.Read(buffer, 0, Convert.ToInt32(stream.Length));
+                        stream.Close();
+                        stream.Dispose();
+                        newBuff = Encoding.Convert(Encoding.GetEncoding("BIG5"), Encoding.UTF8, buffer);
+                        info.Delete();
+                        stream = info.OpenWrite();
+                        stream.Write(newBuff, 0, newBuff.Length);
+                        stream.Close();
+                        sum++;
+                        break;
+                    case "GBK":
+                        stream = info.OpenRead();
+                        buffer = new Byte[stream.Length];
+                        stream.Read(buffer, 0, Convert.ToInt32(stream.Length));
+                        stream.Dispose();
+                        newBuff = Encoding.Convert(Encoding.GetEncoding("GBK"), Encoding.UTF8, buffer);
+                        info.Delete();
+                        stream = info.OpenWrite();
+                        stream.Write(newBuff, 0, newBuff.Length);
+                        stream.Close();
+                        sum++;
+                        break;
+                    //case "ASSIC":
+                    //    stream = info.OpenRead();
+                    //    buffer = new Byte[stream.Length];
+                    //    stream.Read(buffer, 0, Convert.ToInt32(stream.Length));
+                    //    stream.Dispose();
+                    //    newBuff = Encoding.Convert(Encoding.ASCII, Encoding.UTF8, buffer);
+                    //    info.Delete();
+                    //    stream = info.OpenWrite();
+                    //    stream.Write(newBuff, 0, newBuff.Length);
+                    //    stream.Close();
+                    //    break;
+                    default:
+                        break;
+                }
+            }
+            MessageBox.Show(@"转换完成，共转换" + sum + @"个字幕文件！");
+
+        }
+    }
+}
